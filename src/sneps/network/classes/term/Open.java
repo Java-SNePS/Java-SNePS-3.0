987dd0dffd19e8dac19684df5a19493826e9f3a2
m-elassar33 <mostafa@192.168.1.5>
2018-03-12 22:38:59 +0200

    Add freeVariables attribute and create a getter for it in the Open class

--- src/sneps/network/classes/term/Open.java
+++ src/sneps/network/classes/term/Open.java
@@ -1,10 +1,23 @@
 package sneps.network.classes.term;
 
+import java.util.LinkedList;
+
+import sneps.network.Node;
+
 public class Open extends Molecular {
 
+	private LinkedList<Node> freeVariables;
+	
 	public Open(String idenitifier) {
 		super(idenitifier);
 	}
 
-
+	/**
+	 * 
+	 * @return the list of free variables dominated by the current 
+	 */
+	public LinkedList<Node> getFreeVariables(){
+		return this.freeVariables;
+	}
+	
 }
