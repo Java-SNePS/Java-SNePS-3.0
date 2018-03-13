7d444d115611a2719b2eb86bcfa7b168b7bf8174
Omar Hossam <moar.ahmed@gmail.com>
2018-03-14 00:26:13 +0200

    Initial controller shape.
    Not sure about the handling of assertion to context.
    But this commit resolves issue #10 and #8

--- src/sneps/snebr/Controller.java
+++ src/sneps/snebr/Controller.java
@@ -7,8 +7,58 @@ public class Controller {
     private static String currContext = "default";
     private static ContextSet contextSet = new ContextSet(currContext);
 
-    public static void addContext(String contextName) {
+    public static Context addContext(String contextName) {
         Context c = new Context(contextSet.getContext(currContext));
+        c.addName(contextName);
+        contextSet.add(c);
+        return c;
+    }
+
+    public static void updateContextSet(Context c) {
         contextSet.add(c);
     }
+
+    public static Context addPropToContext(PropositionNode p, String contextName) {
+        Context oldContext =  contextSet.getContext(contextName);
+        Context newContext;
+
+        if (oldContext != null) {
+            oldContext.removeName(contextName);
+            newContext = new Context(oldContext.getHypothesisSet());
+            newContext.addName(contextName);
+            contextSet.add(newContext);
+        } else {
+            newContext = new Context(contextSet.getContext(currContext).getHypothesisSet());
+            newContext = newContext.addProp(p);
+            newContext.addName(contextName);
+            contextSet.add(newContext);
+        }
+
+        return newContext;
+    }
+
+//    public static Context addPropToContext(PropositionNode p, Context context) {
+//        updateContextSet(c);
+//        return c;
+//    }
+
+    public Context addPropToCurrentContext(PropositionNode p) {
+        return addPropToContext(p, currContext);
+    }
+
+    public Context setCurrentContext(String contextName) {
+        currContext = contextName;
+        Context context =  contextSet.getContext(contextName);
+        if (context == null) {
+            context = addContext(contextName);
+        }
+        return  context;
+    }
+
+    public static void checkForContradiction(Context c){
+        // TODO: 13/03/18
+    }
+
+
+
 }
