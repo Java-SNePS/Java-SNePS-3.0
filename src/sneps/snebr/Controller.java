ff68298b3f340bb72534262e0a6c674b41e2be67
Omar Hossam <moar.ahmed@gmail.com>
2018-03-13 14:31:13 +0200

    issue #8 resolved

--- src/sneps/snebr/Controller.java
+++ src/sneps/snebr/Controller.java
@@ -1,4 +1,14 @@
 package sneps.snebr;
 
+import sneps.network.PropositionNode;
+import sneps.network.classes.setClasses.ContextSet;
+
 public class Controller {
+    private static String currContext = "default";
+    private static ContextSet contextSet = new ContextSet(currContext);
+
+    public static void addContext(String contextName) {
+        Context c = new Context(contextSet.getContext(currContext));
+        contextSet.add(c);
+    }
 }
