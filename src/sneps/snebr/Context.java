a6087a3b0af80b8a3a503e0598ab83d43539df39
Omar Hossam <moar.ahmed@gmail.com>
2018-03-14 00:53:38 +0200

    A proposed shape of the context class. This aims to resolve issues #14, #15

--- src/sneps/snebr/Context.java
+++ src/sneps/snebr/Context.java
@@ -34,6 +34,31 @@ public class Context {
 		return names;
 	}
 
+	/**
+	 * Creates a new Context with the propositionNode
+	 * @param hyp Propsosition Node to be added to the context's hyps
+	 * @return <code>true</code> if the hyp isn't a duplicate <code>false</code> otherwise.
+	 */
+	public Context addProp(PropositionNode hyp) {
+		Context newContext = new Context(this);
+		newContext.getHypothesisSet().add(hyp);
+		// TODO: check for contradiciton
+		return newContext;
+	}
+
+	public Context addProps(HashSet<PropositionNode> hyps) {
+		Context newContex = new Context(this);
+		newContex.getHypothesisSet().addAll(hyps);
+		// TODO: check for contradiciton
+		return newContex;
+	}
+
+	public Context removeProp(PropositionNode hyp) {
+		Context newContext = new Context(this);
+		newContext.getHypothesisSet().remove(hyp);
+		// TODO: 13/03/18 check for contradiciton
+		return newContext;
+	}
 
 	/**
 	 * Adds a name to the set of names of the context if not a duplicate.
@@ -42,6 +67,10 @@ public class Context {
 	 */
 	public boolean addName(String name) {
 		return this.names.add(name);
+	}
+
+	public boolean addNames(HashSet<String> names) {
+		return this.names.addAll(names);
 	}
 
 	/**
