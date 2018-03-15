452b5c4dc2ffb45524892768de77e215434a4f8f
Omar Hossam <moar.ahmed@gmail.com>
2018-03-15 21:51:39 +0200

    Added wrapper methods to interact with PropositNode hashset

--- src/sneps/network/classes/setClasses/PropositionSet.java
+++ src/sneps/network/classes/setClasses/PropositionSet.java
@@ -12,17 +12,44 @@ public class PropositionSet implements Iterable<PropositionNode> {
 		nodes = new HashSet<PropositionNode>();
 	}
 
+	public PropositionSet(Set<PropositionNode> nodes) {
+		this.nodes = nodes;
+	}
+
 	@Override
 	public Iterator<PropositionNode> iterator() {
 		return nodes.iterator();
 	}
 
+	public Set<PropositionNode> getNodes() {
+		return nodes;
+	}
+
+	public boolean add (PropositionNode p) {
+		return nodes.add(p);
+	}
+
+	public boolean addAll(Set<PropositionNode> props) {
+		return nodes.addAll(props);
+	}
+
+	public boolean remove(PropositionNode prop) {
+		return nodes.remove(prop);
+	}
 
 	@Override
 	public boolean equals(Object obj) {
 		PropositionSet props = (PropositionSet)obj;
 		for (PropositionNode node: props.nodes) {
 			if(!this.nodes.contains(node))
+				return false;
+		}
+		return true;
+	}
+
+	public boolean isSubSet(PropositionSet hyps) {
+		for (PropositionNode node: this.nodes) {
+			if (!hyps.nodes.contains(node))
 				return false;
 		}
 		return true;
