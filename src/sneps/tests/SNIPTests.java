cd0a1cfcce96652e66a0be6f6ee3fab7abaec9a8
ObsoleteAmigo <amagido2@gmail.com>
2018-03-28 15:24:33 +0200

    Testing Binding Class

--- src/sneps/tests/SNIPTests.java
+++ src/sneps/tests/SNIPTests.java
@@ -2,23 +2,37 @@ package sneps.tests;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
+import sneps.network.Node;
+import sneps.network.VariableNode;
+import sneps.snip.matching.Binding;
+
 public class SNIPTests {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
+	private Binding binding; 
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//Binding class tests
+		Node n1 = new Node();
+		VariableNode vn1 = new VariableNode();
+		binding = new Binding(vn1, n1);
+		
+		Node n2 = new Node();
+		VariableNode vn2 = new VariableNode();
+		Binding binding1 = new Binding(vn1, n1);
+		//IsEqual tests
+		assertTrue("First Binding is not equal to binding1", binding.isEqual(binding1));
+		
+		binding1.setNode(n2);
+		binding1.setVariable(vn2);
+		
+		assertFalse("Second Binding is not equal to binding1", binding.isEqual(binding1));
+		//Clone Tests
+		binding1 = binding.clone();
+		assertTrue("Clone binding1 is not equal to binding", binding.isEqual(binding1));
+		
+		//fail("Not yet implemented");
 	}
 
 }
