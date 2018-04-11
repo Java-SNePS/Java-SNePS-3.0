70b8a487b47d598ed377158a1d37faa63dd9bd14
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-04-11 13:01:38 +0200

    Add the executeSnepslogCommand method to the AP class

--- src/sneps/snepslog/AP.java
+++ src/sneps/snepslog/AP.java
@@ -1,5 +1,24 @@
 package sneps.snepslog;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import java_cup.runtime.Symbol;
+
+@SuppressWarnings("deprecation")
 public class AP {
 
+	public static String executeSnepslogCommand(String command) throws Exception{
+		InputStream is = new ByteArrayInputStream(command.getBytes(StandardCharsets.UTF_8));
+		DataInputStream dis = new DataInputStream(is);
+		parser parser = new parser(new Lexer(dis));
+		Symbol res = parser.parse();
+		String output = (String) res.value;
+		is.close();
+		dis.close();
+		return output;
+	}
+	
 }
