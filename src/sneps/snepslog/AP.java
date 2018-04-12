5c103ef395765427e54f2770a68c07f30ae54acc
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-04-12 16:11:54 +0200

    Create the createModeOneCaseFrame method in the AP class

--- src/sneps/snepslog/AP.java
+++ src/sneps/snepslog/AP.java
@@ -4,12 +4,28 @@ import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
 
 import java_cup.runtime.Symbol;
+import sneps.exceptions.CustomException;
+import sneps.network.Network;
+import sneps.network.classes.CaseFrame;
+import sneps.network.classes.Relation;
 
 @SuppressWarnings("deprecation")
 public class AP {
 
+	public static CaseFrame createModeOneCaseFrame(int noOfArguments) throws CustomException {
+		LinkedList<Relation> rels = new LinkedList<Relation>();
+		Relation r = new Relation("r", "Entity");
+		rels.add(r);
+		for(int i=0;i<noOfArguments;i++) {
+			rels.add(new Relation("a"+(i+1), "Entity"));
+		}
+		CaseFrame cf = Network.defineCaseFrame("Entity", rels);
+		return cf;
+	}
+	
 	public static String executeSnepslogCommand(String command) throws Exception{
 		InputStream is = new ByteArrayInputStream(command.getBytes(StandardCharsets.UTF_8));
 		DataInputStream dis = new DataInputStream(is);
