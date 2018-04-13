0bfc2dc66238e2809532c2bb64b24932b2fce685
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-04-13 20:54:31 +0200

    Document everything in the AP class

--- src/sneps/snepslog/AP.java
+++ src/sneps/snepslog/AP.java
@@ -1,3 +1,13 @@
+/**
+ * @className AP.java
+ * 
+ * @ClassDescription This is the class that acts as an interface to the snepslog 
+ *  parser. It contains some static fields and some helper methods used to make 
+ *  changes in the backend.
+ * 
+ * @author Mostafa El-assar
+ * @version 3.00 1/4/2018
+ */
 package sneps.snepslog;
 
 import java.io.ByteArrayInputStream;
@@ -15,25 +25,57 @@ import sneps.network.classes.Relation;
 @SuppressWarnings("deprecation")
 public class AP {
 
+	/**
+	 * an integer which holds the number of the snepslog mode currently in use. It 
+	 * is initially set to 1.
+	 */
 	private static int snepslogMode = 1;
+	
+	/**
+	 * a String which holds the name of the printing mode currently in use. It 
+	 * is initially set to normal.
+	 */
 	private static String printingMode = "normal";
 
+	/**
+	 * @return an int representing the number of the snepslog mode currently in use.
+	 */
 	protected static int getSnepslogMode() {
 		return snepslogMode;
 	}
 
+	/**
+	 * @param snepslogMode
+	 *            the number of the snepslog mode to be used.
+	 */
 	protected static void setSnepslogMode(int snepslogMode) {
 		AP.snepslogMode = snepslogMode;
 	}
 
+	/**
+	 * @return a String representing the name of the printing mode currently in use.
+	 */
 	protected static String getPrintingMode() {
 		return printingMode;
 	}
 
+	/**
+	 * @param printingMode
+	 *            the name of the printing mode to be used.
+	 */
 	protected static void setPrintingMode(String printingMode) {
 		AP.printingMode = printingMode;
 	}
 
+	/**
+	 * This method is create a customized case frame for mode 1.
+	 *
+	 * @param noOfArguments
+	 *            the number of argument relations.
+	 *
+	 * @throws CustomException
+	 *             if the case frame is already created.
+	 */
 	protected static CaseFrame createModeOneCaseFrame(int noOfArguments) throws CustomException {
 		LinkedList<Relation> rels = new LinkedList<Relation>();
 		Relation r = new Relation("r", "Entity");
@@ -45,6 +87,18 @@ public class AP {
 		return cf;
 	}
 
+	/**
+	 * This method is create a customized case frame for mode 2.
+	 *
+	 * @param p
+	 *            the name of the p relation.
+	 *
+	 * @param noOfArguments
+	 *            the number of argument relations.
+	 *
+	 * @throws CustomException
+	 *             if the case frame is already created.
+	 */
 	protected static CaseFrame createModeTwoCaseFrame(String p, int noOfArguments) throws CustomException {
 		LinkedList<Relation> rels = new LinkedList<Relation>();
 		Relation r = new Relation("| rel " + p + "|", "Entity");
@@ -56,6 +110,17 @@ public class AP {
 		return cf;
 	}
 
+	/**
+	 * This method is used to execute a snepslog command.
+	 * 
+	 * @param command
+	 *            a String holding the command that is to be executed.
+	 * 
+	 * @return a String representing the output of that command.
+	 * 
+	 * @throws Exception
+	 *             if the command is syntactically incorrect.
+	 */
 	public static String executeSnepslogCommand(String command) throws Exception {
 		InputStream is = new ByteArrayInputStream(command.getBytes(StandardCharsets.UTF_8));
 		DataInputStream dis = new DataInputStream(is);
