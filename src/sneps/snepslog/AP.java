7e622071e2dfa615f622e351de7ae28db03371e8
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-04-18 17:17:32 +0200

    Create the buildEntailment method in AP

--- src/sneps/snepslog/AP.java
+++ src/sneps/snepslog/AP.java
@@ -14,13 +14,18 @@ import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 import java_cup.runtime.Symbol;
 import sneps.exceptions.CustomException;
 import sneps.network.Network;
+import sneps.network.Node;
 import sneps.network.classes.CaseFrame;
 import sneps.network.classes.Relation;
+import sneps.network.classes.RelationsRestrictedCaseFrame;
+import sneps.network.classes.Semantic;
+import sneps.network.classes.Wire;
 
 @SuppressWarnings("deprecation")
 public class AP {
@@ -162,6 +167,78 @@ public class AP {
 	 */
 	protected static void clearKnowledgeBase() {
 		// TODO Finish building clearKnowledgeBase()
+	}
+
+	/**
+	 * This method is used to construct the nodes representing entailments in the
+	 * network.
+	 * 
+	 * @param entailmentType
+	 *            a String specifying the type of the entailment. It should have one
+	 *            of the following values: AndEntailment, OrEntailment,
+	 *            NumericalEntailment or Implication.
+	 * @param antecedents
+	 *            an ArrayList of the nodes representing the antecedents.
+	 * @param consequents
+	 *            an ArrayList of the nodes representing the consequents.
+	 * @param optionalI
+	 *            a String which contains the value of "i" in case of a numerical
+	 *            entailment.
+	 * @return a molecular node representing the entailment
+	 * @throws Exception
+	 */
+	protected static Node buildEntailment(String entailmentType, ArrayList<Node> antecedents,
+			ArrayList<Node> consequents, String optionalI) throws Exception {
+		RelationsRestrictedCaseFrame.createDefaultCaseFrames();
+		RelationsRestrictedCaseFrame caseFrame = null;
+		ArrayList<Wire> wires = new ArrayList<Wire>();
+		switch (entailmentType) {
+		case "AndEntailment":
+			caseFrame = (RelationsRestrictedCaseFrame) RelationsRestrictedCaseFrame.andRule;
+			for (int i = 0; i < antecedents.size(); i++) {
+				wires.add(new Wire(Relation.andAnt, antecedents.get(i)));
+			}
+			for (int j = 0; j < consequents.size(); j++) {
+				wires.add(new Wire(Relation.cq, consequents.get(j)));
+			}
+			break;
+		case "OrEntailment":
+			caseFrame = (RelationsRestrictedCaseFrame) RelationsRestrictedCaseFrame.orRule;
+			for (int i = 0; i < antecedents.size(); i++) {
+				wires.add(new Wire(Relation.ant, antecedents.get(i)));
+			}
+			for (int j = 0; j < consequents.size(); j++) {
+				wires.add(new Wire(Relation.cq, consequents.get(j)));
+			}
+			break;
+		case "NumericalEntailment":
+			caseFrame = (RelationsRestrictedCaseFrame) RelationsRestrictedCaseFrame.numericalRule;
+			for (int i = 0; i < antecedents.size(); i++) {
+				wires.add(new Wire(Relation.andAnt, antecedents.get(i)));
+			}
+			for (int j = 0; j < consequents.size(); j++) {
+				wires.add(new Wire(Relation.cq, consequents.get(j)));
+			}
+			//possible conflict in identifiers
+			// Check semantic and syntactic type
+			wires.add(new Wire(Relation.i, Network.buildBaseNode(optionalI, new Semantic("Individual"))));
+			break;
+		case "Implication":
+			//check the case frame in use
+			caseFrame = (RelationsRestrictedCaseFrame) RelationsRestrictedCaseFrame.numericalRule;
+			for (int i = 0; i < antecedents.size(); i++) {
+				wires.add(new Wire(Relation.andAnt, antecedents.get(i)));
+			}
+			for (int j = 0; j < consequents.size(); j++) {
+				wires.add(new Wire(Relation.cq, consequents.get(j)));
+			}
+			//possible conflict in identifiers
+			// Check semantic and syntactic type
+			wires.add(new Wire(Relation.i, Network.buildBaseNode("1", new Semantic("Individual"))));
+			break;
+		}
+		Node entailmentNode = Network.buildMolecularNode(wires, caseFrame);
+		return entailmentNode;
 	}
 
 	/**
