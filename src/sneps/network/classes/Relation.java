fcd51e8a7b141cae49664585fab77883c9c9d854
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-04-21 22:23:02 +0200

    Add the relation suchthat in the Relation class

--- src/sneps/network/classes/Relation.java
+++ src/sneps/network/classes/Relation.java
@@ -19,7 +19,8 @@ import sneps.network.paths.Path;
 public class Relation implements Serializable {
 
 	public static Relation andAnt, ant, cq, arg, min, max, i, threshMax, thresh, action, obj, precondition, act, when,
-			whenever, doo, iff, effect, plan, goal, obj1, obj2, obj3, obj4, obj5, obj6, obj7, obj8, obj9, obj10;
+			whenever, doo, iff, effect, plan, goal, suchthat, obj1, obj2, obj3, obj4, obj5, obj6, obj7, obj8, obj9,
+			obj10;
 
 	/**
 	 * the name (string) that should label any arc representing this relation. Any
@@ -224,6 +225,7 @@ public class Relation implements Serializable {
 		plan = Network.defineRelation("plan", "Act", "none", 1);
 		goal = Network.defineRelation("goal", "Proposition", "none", 1);
 		effect = Network.defineRelation("effect", "Proposition", "none", 1);
+		suchthat = Network.defineRelation("suchthat", "Proposition", "none", 1);
 	}
 
 }
