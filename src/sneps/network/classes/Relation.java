cd32af3db03d3fd767fe54ac21d6521fc83c8f5b
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-04-21 22:32:18 +0200

    Create the relation else in the Relation class

--- src/sneps/network/classes/Relation.java
+++ src/sneps/network/classes/Relation.java
@@ -19,8 +19,8 @@ import sneps.network.paths.Path;
 public class Relation implements Serializable {
 
 	public static Relation andAnt, ant, cq, arg, min, max, i, threshMax, thresh, action, obj, precondition, act, when,
-			whenever, doo, iff, effect, plan, goal, vars, suchthat, obj1, obj2, obj3, obj4, obj5, obj6, obj7, obj8,
-			obj9, obj10;
+			whenever, doo, iff, effect, plan, goal, vars, suchthat, elsee, obj1, obj2, obj3, obj4, obj5, obj6, obj7,
+			obj8, obj9, obj10;
 
 	/**
 	 * the name (string) that should label any arc representing this relation. Any
@@ -227,6 +227,7 @@ public class Relation implements Serializable {
 		effect = Network.defineRelation("effect", "Proposition", "none", 1);
 		suchthat = Network.defineRelation("suchthat", "Proposition", "none", 1);
 		vars = Network.defineRelation("vars", "Infimum", "none", 1);
+		elsee = Network.defineRelation("else", "Act", "none", 1);
 	}
 
 }
