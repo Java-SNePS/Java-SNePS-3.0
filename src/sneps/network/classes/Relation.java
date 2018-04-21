bc89886a959e0b0b3a2da9036dea0d45148e952b
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-04-21 22:49:28 +0200

    Create the relation withall in the Relation class

--- src/sneps/network/classes/Relation.java
+++ src/sneps/network/classes/Relation.java
@@ -19,8 +19,8 @@ import sneps.network.paths.Path;
 public class Relation implements Serializable {
 
 	public static Relation andAnt, ant, cq, arg, min, max, i, threshMax, thresh, action, obj, precondition, act, when,
-			whenever, doo, iff, effect, plan, goal, vars, suchthat, elsee, withsome, obj1, obj2, obj3, obj4, obj5, obj6,
-			obj7, obj8, obj9, obj10;
+			whenever, doo, iff, effect, plan, goal, vars, suchthat, elsee, withsome, withall, obj1, obj2, obj3, obj4,
+			obj5, obj6, obj7, obj8, obj9, obj10;
 
 	/**
 	 * the name (string) that should label any arc representing this relation. Any
@@ -229,6 +229,7 @@ public class Relation implements Serializable {
 		vars = Network.defineRelation("vars", "Infimum", "none", 1);
 		elsee = Network.defineRelation("else", "Act", "none", 1);
 		withsome = Network.defineRelation("withsome", "Action", "none", 1);
+		withall = Network.defineRelation("withall", "Action", "none", 1);
 	}
 
 }
