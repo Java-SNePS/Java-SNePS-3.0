9b647ef87f268a1b53729728379ce357902f3d37
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-05-03 15:34:42 +0200

    Remove the precedence and resolve all conflicts in favor of shifting

--- src/sneps/snepslog/cup/MainCup.java
+++ src/sneps/snepslog/cup/MainCup.java
@@ -5,10 +5,13 @@ import java_cup.Main;
 public class MainCup {
 
 	public static void main(String[] args) throws Exception  { 
-		String[] argv = new String[3];
+		String[] argv = new String[5];
 		argv[0] = "-destdir";
 		argv[1] = "src/sneps/snepslog";
-		argv[2] = "src/sneps/snepslog/parser.cup";
+		argv[2] = "-expect";
+		argv[3] = "5";
+		argv[4] = "src/sneps/snepslog/parser.cup";
+		
 		Main.main(argv);
     }
 	
