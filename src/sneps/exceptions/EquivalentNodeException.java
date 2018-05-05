5763d94dfc05544b5c6a9ce8e697dd75c2690a4e
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-05-05 23:24:25 +0200

    Adjust the exceptions in buildMolecularNode

---
+++ src/sneps/exceptions/EquivalentNodeException.java
@@ -0,0 +1,14 @@
+package sneps.exceptions;
+
+import sneps.network.Node;
+
+public class EquivalentNodeException extends Exception {
+
+	private Node equivalentNode;
+
+	public EquivalentNodeException(String message, Node equivalentNode) {
+		super(message);
+		this.equivalentNode = equivalentNode;
+	}
+
+}
