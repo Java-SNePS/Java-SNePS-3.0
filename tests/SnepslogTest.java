a0746435e0f10e418ef16d917d0a2fefd5cc58c3
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-05-09 20:42:00 +0200

    Test building an infixedTerm

--- tests/SnepslogTest.java
+++ tests/SnepslogTest.java
@@ -25,7 +25,9 @@ import sneps.network.Node;
 import sneps.network.classes.CaseFrame;
 import sneps.network.classes.Relation;
 import sneps.network.classes.SemanticHierarchy;
+import sneps.network.classes.setClasses.NodeSet;
 import sneps.network.classes.setClasses.PropositionSet;
+import sneps.network.classes.term.Base;
 import sneps.network.classes.term.Molecular;
 import sneps.network.paths.ComposePath;
 import sneps.network.paths.FUnitPath;
@@ -278,6 +280,51 @@ public class SnepslogTest {
 		assertTrue(nodesDescriptions.size() == 0);
 	}
 
+	@Test
+	public void testInfixedTerm() throws NodeNotFoundInNetworkException {
+		AP.executeSnepslogCommand("dog(Fido) and animal(Fido).");
+		Node n = Network.getNode("M3");
+		Molecular m = (Molecular) n.getTerm();
+		assertTrue(m.getDownCableSet().size()==3);
+		assertTrue(m.getDownCableSet().getDownCable("max").getNodeSet().size()==1);
+		assertTrue(m.getDownCableSet().getDownCable("min").getNodeSet().size()==1);
+		assertTrue(m.getDownCableSet().getDownCable("arg").getNodeSet().size()==2);
+		Node max = m.getDownCableSet().getDownCable("max").getNodeSet().getNode(0);
+		assertTrue(max.getTerm() instanceof Base);
+		assertTrue(max.getIdentifier().equals("2"));
+		assertTrue(max.getSemantic().getSemanticType().equals("Infimum"));
+		Node min = m.getDownCableSet().getDownCable("min").getNodeSet().getNode(0);
+		assertTrue(min.getTerm() instanceof Base);
+		assertTrue(min.getIdentifier().equals("2"));
+		assertTrue(min.getSemantic().getSemanticType().equals("Infimum"));
+		NodeSet args = m.getDownCableSet().getDownCable("ant").getNodeSet();
+		boolean success1 = false;
+		boolean success2 = false;
+		for (int i = 0; i < args.size(); i++) {
+			Node n = args.getNode(i);
+			if (n.getTerm() instanceof Molecular) {
+				Molecular m = (Molecular) n.getTerm();
+				LinkedList<Relation> rels = m.getDownCableSet().getCaseFrame().getRelations();
+				if (rels.size() == 2 && rels.get(0).getName().equals("r") && rels.get(1).getName().equals("a1")) {
+					Node dog = Network.getNode("dog");
+					Node animal = Network.getNode("animal");
+					Node Fido = Network.getNode("Fido");
+					if (m.getDownCableSet().getDownCable("r").getNodeSet().contains(dog)
+							&& m.getDownCableSet().getDownCable("a1").getNodeSet().contains(Fido)) {
+						success1 = true;
+					}
+					if (m.getDownCableSet().getDownCable("r").getNodeSet().contains(animal)
+							&& m.getDownCableSet().getDownCable("a1").getNodeSet().contains(Fido)) {
+						success2 = true;
+					}
+				}
+			}
+		}
+		if (!(success1 && success2)) {
+			fail("failure to build this entailment!}");
+		}
+	}
+	
 	@Test
 	public void testEntailment() throws NodeNotFoundInNetworkException {
 		AP.executeSnepslogCommand("dog(Fido)=>animal(Fido).");
