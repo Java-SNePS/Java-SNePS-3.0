7d337a84a307b4a4ed037d8036c1f88d7ec67d91
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-05-09 03:08:13 +0200

    Fix the toString methods and the upCables

--- src/sneps/network/classes/term/Molecular.java
+++ src/sneps/network/classes/term/Molecular.java
@@ -1,8 +1,14 @@
 package sneps.network.classes.term;
 
 import java.io.Serializable;
+import java.util.Enumeration;
 
+import sneps.network.Node;
+import sneps.network.cables.DownCable;
 import sneps.network.cables.DownCableSet;
+import sneps.network.cables.UpCable;
+import sneps.network.classes.Relation;
+import sneps.network.classes.setClasses.NodeSet;
 
 public class Molecular extends Term implements Serializable{
 	protected DownCableSet downCableSet;
@@ -24,4 +30,41 @@ public class Molecular extends Term implements Serializable{
 	public DownCableSet getDownCableSet() {
 		return this.downCableSet;
 	}
+	
+	/**
+	 * This method overrides the default toString method inherited from the Term class.
+	 * 
+	 * @return a string representing the name of the current node + the to string of the 
+	 * 	down cable set.
+	 */
+	@Override
+	public String toString(){
+		return this.getIdentifier()+":("+this.getDownCableSet().toString()+")";
+	}
+	
+	/**
+	 * A method that adds node object, that have the current molecular
+	 * 	object as its syntactic object, to the suitable up cable of all 
+	 * 	the nodes dominated by the current node.
+	 * 
+	 * @param node
+	 * 			the node object having the current molecular object
+	 * 			as its syntactic object.
+	 */
+	public void updateUpCables(Node node) {
+		DownCableSet dCableSet = this.getDownCableSet();
+		Enumeration<DownCable> dCables = dCableSet.getDownCables().elements();
+		while(dCables.hasMoreElements()){
+			DownCable dCable = dCables.nextElement();
+			Relation r = dCable.getRelation();
+			NodeSet ns = dCable.getNodeSet();
+			for (int j = 0; j < ns.size(); j++){
+				Node n = ns.getNode(j);
+				if (!n.getUpCableSet().contains(r))
+					n.getUpCableSet().addUpCable(new UpCable(r));
+				n.getUpCableSet().getUpCable(r.getName()).addNode(node);
+			}
+		}
+	}
+	
 }
