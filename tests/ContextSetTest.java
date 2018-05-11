fb7ed9df4f97b0fc8712ecefd0f399591af58436
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-05-11 21:30:58 +0200

    Fix a bug in the tests due to the latest changes

--- tests/ContextSetTest.java
+++ tests/ContextSetTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import sneps.exceptions.CustomException;
 import sneps.exceptions.DuplicateContextNameException;
+import sneps.exceptions.IllegalIdentifierException;
 import sneps.exceptions.NodeNotFoundInNetworkException;
 import sneps.exceptions.NotAPropositionNodeException;
 import sneps.network.Network;
@@ -17,46 +18,49 @@ import sneps.snebr.Controller;
 import static org.junit.Assert.*;
 
 public class ContextSetTest {
-    private Context context;
-    private ContextSet contextSet;
-    final static String contextName = "test context";
-    private final Semantic semantic = new Semantic("Proposition");
+	private Context context;
+	private ContextSet contextSet;
+	final static String contextName = "test context";
+	private final Semantic semantic = new Semantic("Proposition");
 
-    @Before
-    public void setUp() throws DuplicateContextNameException, NotAPropositionNodeException, CustomException, NodeNotFoundInNetworkException {
-        for (int i = 0; i < 8889; i++)
-            Network.buildBaseNode("n"+i, semantic);
-        context = Controller.createContext(contextName, new PropositionSet(new int [] {1,3,4}));
-        contextSet = new ContextSet(context);
-    }
+	@Before
+	public void setUp() throws DuplicateContextNameException, NotAPropositionNodeException, CustomException,
+			NodeNotFoundInNetworkException, IllegalIdentifierException {
+		for (int i = 0; i < 8889; i++)
+			Network.buildBaseNode("n" + i, semantic);
+		context = Controller.createContext(contextName, new PropositionSet(new int[] { 1, 3, 4 }));
+		contextSet = new ContextSet(context);
+	}
 
-    @Test
-    public void getContext() {
-        assertEquals(context, contextSet.getContext(contextName));
-    }
+	@Test
+	public void getContext() {
+		assertEquals(context, contextSet.getContext(contextName));
+	}
 
-    @Test
-    public void remove() {
-        contextSet.remove(contextName);
-        assertNull(contextSet.getContext(contextName));
-    }
+	@Test
+	public void remove() {
+		contextSet.remove(contextName);
+		assertNull(contextSet.getContext(contextName));
+	}
 
-    @Test
-    public void add() throws DuplicateContextNameException, NotAPropositionNodeException, CustomException, NodeNotFoundInNetworkException {
-        Context temp = Controller.createContext("temp context", new PropositionSet(new int [] {34,89}));
-        contextSet.add(temp);
-        assertEquals(temp, contextSet.getContext("temp context"));
-    }
+	@Test
+	public void add() throws DuplicateContextNameException, NotAPropositionNodeException, CustomException,
+			NodeNotFoundInNetworkException {
+		Context temp = Controller.createContext("temp context", new PropositionSet(new int[] { 34, 89 }));
+		contextSet.add(temp);
+		assertEquals(temp, contextSet.getContext("temp context"));
+	}
 
-    @Test
-    public void identicalContext() throws DuplicateContextNameException, NotAPropositionNodeException, CustomException, NodeNotFoundInNetworkException {
-        Context c2 = Controller.createContext("context 2", new PropositionSet(new int [] {1,3,4}));
-        assertEquals(contextSet.identicalContext(c2), context);
-    }
+	@Test
+	public void identicalContext() throws DuplicateContextNameException, NotAPropositionNodeException, CustomException,
+			NodeNotFoundInNetworkException {
+		Context c2 = Controller.createContext("context 2", new PropositionSet(new int[] { 1, 3, 4 }));
+		assertEquals(contextSet.identicalContext(c2), context);
+	}
 
-    @After
-    public void removeContext() {
-        Controller.removeContext(contextName);
-    }
+	@After
+	public void removeContext() {
+		Controller.removeContext(contextName);
+	}
 
 }
