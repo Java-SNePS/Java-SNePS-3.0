c0dbfede088605b5b3a1f4750b425d6d153d99d2
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-05-13 20:14:19 +0200

    Resolve the conflicts in the grammar

--- src/sneps/snepslog/cup/MainCup.java
+++ src/sneps/snepslog/cup/MainCup.java
@@ -5,12 +5,10 @@ import java_cup.Main;
 public class MainCup {
 
 	public static void main(String[] args) throws Exception  { 
-		String[] argv = new String[5];
+		String[] argv = new String[3];
 		argv[0] = "-destdir";
 		argv[1] = "src/sneps/snepslog";
-		argv[2] = "-expect";
-		argv[3] = "5";
-		argv[4] = "src/sneps/snepslog/parser.cup";
+		argv[2] = "src/sneps/snepslog/parser.cup";
 		
 		Main.main(argv);
     }
