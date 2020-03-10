a9e2be3c505823bfc6f53a6ab64b689e2a546e7a
Youssef Walid <youssef_walid@hotmail.com>
2020-03-10 20:47:27 +0200

    missing files from master

---
+++ src/sneps/snip/channels/ChannelIdentifier.java
@@ -0,0 +1,79 @@
+package sneps.snip.channels;
+
+import sneps.snip.matching.Substitutions;
+
+public class ChannelIdentifier {
+	int requesterId;
+	int reporterId;
+	String contextName;
+	Substitutions filterSubstitutions;
+	Substitutions switchSubstitutions;
+
+	public ChannelIdentifier(int rqId, int rpId, String cName, Substitutions fSubs, Substitutions sSubs) {
+		requesterId = rqId;
+		reporterId = rpId;
+		contextName = cName;
+		filterSubstitutions = fSubs;
+		switchSubstitutions = sSubs;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		ChannelIdentifier channelId;
+		if (obj instanceof ChannelIdentifier) {
+			channelId = (ChannelIdentifier) obj;
+			boolean requesterCheck = getRequesterId() == channelId.getRequesterId();
+			boolean reporterCheck = getReporterId() == channelId.getReporterId();
+			boolean contextCheck = getContextName() == channelId.getContextName();
+			boolean filterCheck = filterSubstitutions.equals(channelId.getFilterSubstitutions());
+			boolean switchCheck = switchSubstitutions.equals(channelId.getSwitchSubstitutions());
+			return reporterCheck && requesterCheck && contextCheck && filterCheck && switchCheck;
+		}
+		return false;
+	}
+
+	public String getContextName() {
+		return contextName;
+	}
+
+	public void setContextName(String contextName) {
+		this.contextName = contextName;
+	}
+
+	public int getRequesterId() {
+		return requesterId;
+	}
+
+	public void setRequesterId(int requesterId) {
+		this.requesterId = requesterId;
+	}
+
+	public int getReporterId() {
+		return reporterId;
+	}
+
+	public void setReporterId(int reporterId) {
+		this.reporterId = reporterId;
+	}
+
+	public Substitutions getFilterSubstitutions() {
+		return filterSubstitutions;
+	}
+
+	public void setFilterSubstitutions(Substitutions filterSubstitutions) {
+		this.filterSubstitutions = filterSubstitutions;
+	}
+
+	public Substitutions getSwitchSubstitutions() {
+		return switchSubstitutions;
+	}
+
+	public void setSwitchSubstitutions(Substitutions switchSubstututions) {
+		this.switchSubstitutions = switchSubstututions;
+	}
+
+	public String toString() {
+		return requesterId + "" + reporterId + contextName + filterSubstitutions.toString()
+				+ switchSubstitutions.toString();
+	}
+}
---
+++ src/sneps/snip/classes/RuleResponse.java
@@ -0,0 +1,43 @@
+package sneps.snip.classes;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import sneps.network.classes.setClasses.ChannelSet;
+import sneps.network.classes.setClasses.ReportSet;
+import sneps.snip.Report;
+import sneps.snip.channels.Channel;
+
+public class RuleResponse {
+
+	private Report report;
+	private Collection<Channel> consequentChannels;
+
+	public RuleResponse() {
+		consequentChannels = new ArrayList<Channel>();
+	}
+
+	public Report getReport() {
+		return report;
+	}
+
+	public void setReport(Report report) {
+		this.report = report;
+	}
+
+	public void addReport(Report report) {
+		this.report = report;
+	}
+
+	public Collection<Channel> getConsequentChannels() {
+		return consequentChannels;
+	}
+
+	public void setConsequentChannels(Collection<Channel> consequentChannels) {
+		this.consequentChannels = consequentChannels;
+	}
+
+	public void addChannel(Channel channel) {
+		this.consequentChannels.add(channel);
+	}
+}
